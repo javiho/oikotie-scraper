f526560d0c747ec72b890590c68c89286f7f8ec9
Jaakko Honkanen <javiho@utu.fi>
2017-02-09 21:24:50 +0200

    can save to database

--- apartement_scraping.py
+++ apartement_scraping.py
@@ -3,16 +3,19 @@ import requests
 import json as js_library
 import io
 import sqlite3
+import collections
 
 """
 Kehitysehdotus: estä SQL-injektiohyökkäykset.
 """
 
-# New database is created if one doesn't exist.
-conn = sqlite3.connect('testdb.db')
-print("opened database")
-conn.close()
-print("databse connection closed")
+def from_site_to_databse():
+    all_cards = []
+    limit = 24
+    for offset in range(0, 3*limit, limit):
+        cards_subset = get_set_of_cards(offset=offset, limit=limit)
+        all_cards.extend(cards_subset)
+    save_cards_to_db(all_cards)
 
 def get_set_of_cards(offset, limit):
     r = requests.get('https://asunnot.oikotie.fi/api/cards?cardType=100&limit=' + str(limit) +
@@ -34,8 +37,8 @@ def get_card_dictionaries(json):
     return cds
 
 def get_card_dict(json, card_index):
-    d = {}
-    attribute_names = ['id', 'description']#, 'rooms', 'roomConfiguration', 'price', 'size', 'sizeLot']
+    d = collections.OrderedDict()
+    attribute_names = json_attribute_names
     for attribute_name in attribute_names:
         add_attribute_to_dict(d, json, card_index, attribute_name)
     return d
@@ -53,24 +56,51 @@ def price_string_to_int(price_str):
     #print("replaced: ", price_str)
     only_digits = ''.join(filter(lambda x: x.isdigit(), price_str))
     as_int = int(only_digits)
-    print(as_int)
+    #print(as_int)
     return as_int
 
+def save_cards_to_db(card_dicts):
+    # New database is created if one doesn't exist.
+    conn = sqlite3.connect(db_path)
+    print("opened database")
+    for card in card_dicts:
+        insert_card(conn, card)
+    conn.commit()
+    conn.close()
+    print("database connection closed")
+    
+def insert_card(conn, card_dict):
+    attr_values_list = [val for val in card_dict.values()]
+    conn.execute('INSERT INTO apartment(id, description, rooms, room_configuration, price, size, size_lot) ' +
+                 'VALUES (?, ?, ?, ?, ?, ?, ?)', attr_values_list)
+
+def json_attr_to_sql_attr(json_attr_name):
+    """Returns the name of the database table attribute which corresponds to the
+    attribute in card object of the json data.
+    """
+    for json_attr in json_attribute_names:
+        if json_attr_name == json_attr:
+            return json_attr_name
+    raise Exception("Attribute name not found: " + json_attr_name)   
 
 #r = requests.get('https://api.github.com/events')
 apartements_table_attr_names = ['id', 'description', 'rooms', 'room_configuration', 'price', 'size', 'size_lot']
+json_attribute_names = ['id', 'description', 'rooms', 'roomConfiguration', 'price', 'size', 'sizeLot']
+db_path = 'testdb.db'
+
+conn = sqlite3.connect(db_path)
+cursor = conn.execute('SELECT * FROM apartment')
+for row in cursor:
+    print('id: ', row[0])
+    print('description: ', row[1])
 
 #r = requests.get('https://asunnot.oikotie.fi/api/cards?cardType=100&limit=24&offset=0&sortBy=published_desc')
 #r.encoding = 'ISO-8859-1'
 #print(as_json['cards'][0]['price'])
-all_cards = []
-limit = 24
-for offset in range(0, 3*limit, limit):
-    cards_subset = get_set_of_cards(offset=offset, limit=limit)
-    all_cards.extend(cards_subset)
 
-print(all_cards)
-print(len(all_cards))
+
+#print(all_cards)
+#print(len(all_cards))
 #print("cd count: ", len(card_dictionaries))
 #print(card_dictionaries)
 
